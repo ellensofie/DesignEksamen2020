90c53386a8d8dd0cbcc9259deaa69a9d14d1e67e
Ellen-Sofie <elch@itu.dk>
2020-05-09 10:55:38 +0200

    Dårlige farver men alligevel

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-function changePage(){
-    window.location.href = "nextpage.html";
-}
 
-function todaysDate(){
-    var d = new Date(); 
-    var date = d.getDay() + '-' + (d.getMonth()+1) + '-' + d.getFullYear();
-    document.getElementById("dateDisplay").innerHTML = date;
+function getDate(){
+    var dt = new Date();
+    document.getElementById("datetime").innerHTML = dt.toLocaleString();
 }
 
 function loadNextPage(){
