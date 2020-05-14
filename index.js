230834a966d3b73d75ee91f693f7c877d36f4c25
lipw <lipw@itu.dk>
2020-05-14 17:43:17 +0200

    Carousel virker

--- index.js
+++ index.js
@@ -24,3 +24,10 @@ function updateimage(){
     var file = document.querySelector('input[type=file]').files[0];
     img.src = window.URL.createObjectURL(file);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instance = M.Carousel.init({
+        fullWidth: true
+    });
+});
