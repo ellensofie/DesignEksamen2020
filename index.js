62f98ca4bfb7406c0c685028f7b86efffd65fd93
Laura <lbje@itu.dk>
2020-05-14 14:15:48 +0200

    load of picture fixed

--- index.js
+++ index.js
@@ -19,8 +19,14 @@ function loadViewPage(){
     window.location.href = "view.html";
 }
 
-function updateimage(){ 
-    var img = document.getElementById('meme-image');
+function updateimage(){
+    var img = document.getElementById('preview-image');
     var file = document.querySelector('input[type=file]').files[0];
     img.src = window.URL.createObjectURL(file);
+    localStorage.setItem('image',JSON.stringify(img.src));
+}
+
+function loadimage(){
+    let img = document.getElementById('page-image');
+    img.src = JSON.parse(localStorage.getItem('image'));
 }
